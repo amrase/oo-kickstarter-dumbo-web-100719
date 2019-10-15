9ca39042727a932c87e6290573c93da565399670
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-15 05:22:07 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,11 @@
 class Backer 
-  attr_reader :name , :backed_project
+  attr_reader :name , :backed_projects
   def initialize(name)
     @name=name
     @backed_project=[]
   end  
   
-  def backed_project(project)
+  def backed_projects(project)
     @backed_project << self 
     project.backed_project = self
   end 
